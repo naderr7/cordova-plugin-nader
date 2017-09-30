213fddef27e3e4bce4b6cc00fc303b8b76e37f24
Nader <nader.nakheel@gmail.com>
2017-09-30 18:57:33 +0300

    Create naderr.h

---
+++ src/ios/naderr.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface naderPlugin : CDVPlugin
+
+// Prints the content
+- (void) print:(CDVInvokedUrlCommand*)command;
+// Find available printers and return founded printers array
+- (void) getAvailablePrinters:(CDVInvokedUrlCommand*)command;
+
+@end
